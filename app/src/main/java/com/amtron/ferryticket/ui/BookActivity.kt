234b41e0c2f499fe4034e9bfc95614f8d76c0249
Debabrata10 <debabrata.sarma2012@gmail.com>
2023-02-25 05:45:19 +0530

    added ui logic to open and close add vehicle number

--- app/src/main/java/com/amtron/ferryticket/ui/BookActivity.kt
+++ app/src/main/java/com/amtron/ferryticket/ui/BookActivity.kt
@@ -1,7 +1,11 @@
 package com.amtron.ferryticket.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.amtron.ferryticket.databinding.ActivityBookBinding
 
@@ -9,6 +13,7 @@ class BookActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBookBinding
     private var isAddPassengerCardVisible: Boolean = false
     private var isAddVehicleCardVisible: Boolean = false
+    private var isAddVehicleNumberVisible: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +47,20 @@ class BookActivity : AppCompatActivity() {
             if (isAddVehicleCardVisible) {
                 isAddVehicleCardVisible = false
                 binding.addVehicleCard.visibility = View.GONE
+            }
+        }
+
+        binding.rgVehicleType.setOnCheckedChangeListener { _, checkedId ->
+            val rb: RadioButton = findViewById(checkedId)
+            Log.d("id", rb.text.toString())
+            if (rb.text.toString() == "Motor Cycle" ||
+                rb.text.toString() == "LMV" ||
+                rb.text.toString() == "HMV") {
+                isAddVehicleNumberVisible = true
+                binding.textInputLayout5.visibility = View.VISIBLE
+            } else {
+                isAddVehicleNumberVisible = false
+                binding.textInputLayout5.visibility = View.GONE
             }
         }
     }
